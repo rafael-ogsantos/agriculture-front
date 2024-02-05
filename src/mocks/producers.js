895a31fb89a219ffd4dcae77fd16c69b49a0ef83
Galvao <rafaeloliveira@febracis.com.br>
2024-02-05 14:41:08 -0300

    producer mock

---
+++ src/mocks/producers.js
@@ -0,0 +1,32 @@
+export const mockProducers = [
+    {
+        id: 1,
+        name: 'Produtor 1',
+        farm: 'Fazenda 1',
+        totalArea: 1000,
+        arableArea: 500,
+        vegetationArea: 500,
+        state: 'SP',
+        crop: ['Coffee'],
+    },
+    {
+        id: 2,
+        name: 'Produtor 2',
+        farm: 'Fazenda 2',
+        totalArea: 2000,
+        arableArea: 1000,
+        vegetationArea: 1000,
+        state: 'MG',
+        crop: ['Coffee'],
+    },
+    {
+        id: 3,
+        name: 'Produtor 3',
+        farm: 'Fazenda 3',
+        totalArea: 3000,
+        arableArea: 1500,
+        vegetationArea: 1500,
+        state: 'PR',
+        crop: ['Corn'],
+    }
+]
